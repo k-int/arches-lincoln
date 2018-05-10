ed12bfebf68d083af5fca932d4f8d09648d7c48f
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-05-10 19:46:59 +0100

    Clear the local settings.

--- lincoln/lincoln/settings_local.py
+++ lincoln/lincoln/settings_local.py
@@ -3,31 +3,3 @@ Django settings for lincoln project.
 """
 
 import os
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-# If running in docker, ensure that these settings match those in your docker compose file.
-ALLOWED_HOSTS = ['localhost', '10.0.2.2']
-DATABASES = {
-    "default": {
-        "ATOMIC_REQUESTS": False,
-        "AUTOCOMMIT": True,
-        "CONN_MAX_AGE": 0,
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "HOST": "db",
-        "NAME": "arches",
-        "OPTIONS": {},
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "PORT": "5432",
-        "POSTGIS_TEMPLATE": "template_postgis_20",
-        "TEST": {
-            "CHARSET": None,
-            "COLLATION": None,
-            "MIRROR": None,
-            "NAME": None
-        },
-        "TIME_ZONE": None
-    }
-}
