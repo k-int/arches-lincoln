f752ef138bc559293cd6e937c531248b7756579a
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-05-15 19:58:44 +0100

    Try with mount points.

--- lincoln/lincoln/settings.py
+++ lincoln/lincoln/settings.py
@@ -90,7 +90,7 @@ LOGGING = {   'disable_existing_loggers': False,
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 
-MEDIA_ROOT =  os.path.join(APP_ROOT)
+MEDIA_ROOT =  os.path.join(APP_ROOT, 'files')
 
 TILE_CACHE_CONFIG = {
     "name": "Disk",
