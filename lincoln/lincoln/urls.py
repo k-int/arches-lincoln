d912fcc5617c15ec730604461653714711aefc35
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-05-15 20:21:17 +0100

    Doh.

--- lincoln/lincoln/urls.py
+++ lincoln/lincoln/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
+urlpatterns = [
+    url(r'^', include('arches.urls')),
+]
+
 if settings.DEBUG:
-    urlpatterns = [
-        url(r'^', include('arches.urls')),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
